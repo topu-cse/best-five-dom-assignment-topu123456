8d4361da572eda9a9560542225393001b0affa06
topu12345 <topu15-3604@diu.edu.bd>
2022-08-23 02:53:19 +0600

    commit name

--- js/Selected.js
+++ js/Selected.js
@@ -8,6 +8,14 @@ function getPlayerName(btnId, pleyerid) {
         li1.innerText = addplayername; 
         inputplayer.appendChild(li1); 
          
+        let player=inputplayer.childNodes.length;
+        if (player > 6) { 
+            alert('ONLY FIVE PLAYER ALLOW'); 
+            inputplayer.removeChild(li1); 
+        } 
+        else { 
+            return; 
+        } 
  
     }) 
 } 
@@ -17,5 +25,40 @@ const ronaldo = getPlayerName('ronaldo-btn', 'ronaldo');
 const luis = getPlayerName('luis-btn', 'luis');
 const vinicius = getPlayerName('jr-btn', 'jr') 
 const ozil = getPlayerName('ozil-btn', 'ozil'); 
+
+
+
+
+document.getElementById('playerid-oneplayer').addEventListener('click', function () { 
+    const playerfile = document.getElementById('perPlayer-cost'); 
+    const costValue = playerfile.value; 
+    const selectedPlayersCost = document.getElementById('serial-inputName').childNodes.length - 1; 
+    const playersExpences = costValue * selectedPlayersCost; 
+    const playersCostFeild = document.getElementById('totalplayer-cost'); 
+    playersCostFeild.innerText = playersExpences; 
+})
+
+
+document.getElementById('all-total-btn').addEventListener('click',function(){
+    const managerCostField = document.getElementById('manager-cost'); 
+    const managerCoststing = managerCostField.value;
+   const managercosttotal =parseFloat(managerCoststing)
+    //console.log(managercosttotal)
+    const coachCostField = document.getElementById('coach-cost'); 
+    const coachCoststing = coachCostField.value;
+    const coachtotalcost=parseFloat(coachCoststing) 
+    //console.log(coachtotalcost)
+    const playerscostForHireField = document.getElementById('totalplayer-cost'); 
+    const playerscostForHire = parseInt(playerscostForHireField.innerText); 
+ 
+    
+
+    const totalField = document.getElementById('total');
+   //const totalFieldsting=totalField.innerText;
+//    const totalvalue=parseFloat(totalFieldsting)
+  const totalvalue =playerscostForHire + managercosttotal + coachtotalcost ;
+  totalField.innerText = totalvalue;
+    
+})
  
  
